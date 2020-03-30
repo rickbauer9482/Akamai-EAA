70292c2114cea5aa5e90ac5b283c97eae4e98605
Rick Bauer <rbauer9482@ptd.net>
2020-03-30 14:12:21 -0400

    Update EAA-Bulk-RDP-App.py

--- EAA-Bulk-RDP-App.py
+++ EAA-Bulk-RDP-App.py
@@ -1,6 +1,7 @@
 from eaa.client import Client
 from eaa.common import *
 from sys import argv
+import time
 
 client = Client(api_key='XXXXXXXX', secret_key='YYYYYYYYY')
 apps = client.applications
@@ -21,7 +22,7 @@ for line in fl:
         domain=Domain.APP_DOMAIN_WAPP, 
         app_profile=AppProfile.APP_PROFILE_RDP, 
         pop_region=fh[2], 
-        description='This application was created by EAA-Bulk-RDP.py')
+        description='This application was created by Akamai-EAA-App-Create.py')
 
     # Add App Servers
     app.servers.add(
@@ -39,19 +40,20 @@ for line in fl:
     app.groups.add(directory_name=fh[7], group_names=[fh[8]])
 
     # Add users to the Overlay group
-    dr = directories.get(name=fh[7])
-    usr = dr.users.get(name=''+fh[9]+'')
-    grp = dr.groups.get(name=fh[8])
-    usr.add_groups([grp])
+    spgi_dr = directories.get(name=fh[7])
+    spgi_usr = spgi_dr.users.get(name=fh[9])
+    spgi_grp = spgi_dr.groups.get(name=fh[8])
+    spgi_usr.add_groups([spgi_grp])
+    
+
     # Configure Services
     app.services.add(service_type=ServiceType.SERVICE_TYPE_ACCELERATION)
 
     #Configure RDP Advanced Settings
     app.set_remote_spark_advanced_settings(remote_spark_mapClipboard=False, remote_spark_mapDisk=False, remote_spark_disk=None, remote_spark_mapPrinter=False, remote_spark_audio=False)
 
-    print "Application Creation Complete."
+    print "Application Creation Complete for:", fh[0]
     app.deploy()
-    app.get_deploy_status()
-
-
-
+    time.sleep(10)
+    # app.get_deploy_status()
+print "***We are all done, please test application access***"
